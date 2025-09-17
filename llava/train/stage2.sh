6cea842d79a423a03b7cdc0a9a5891cd1ff65b80
dingfu.lu@innopeaktech.com <s9059060@a100cs1.innopeaktech.com>
2025-09-16 23:20:22 -0700

    stage2 trainning script

--- llava/train/stage2.sh
+++ llava/train/stage2.sh
@@ -2,22 +2,22 @@
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero3.json \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path ./playground/data/llava_v1_5_mix665k.json \
-    --image_folder ./playground/data \
+    --data_path /nfs/AI/VideoEnhancement/dingfu/vg/LLaVA/playground/data/llava_pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder /nfs/AI/VideoEnhancement/dingfu/vg/LLaVA/playground/data/llava_pretrain/images \
     --vision_tower facebook/dinov2-base \
     --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-13b-dinov2-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
-    --mm_vision_select_layer -1 \
+    --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-dinov2 \
+    --output_dir ./checkpoints/llava-v1.5-7b-dinov2 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 32 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
