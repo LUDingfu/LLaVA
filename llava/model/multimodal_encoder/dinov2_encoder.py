505aaf0d0a4d320114d611e96c29eb8747dd99d6
Bocheng Zou <bocheng.zou@outlook.com>
2025-10-09 04:42:14 +0000

    Fix the resolution issue in MRF

--- llava/model/multimodal_encoder/dinov2_encoder.py
+++ llava/model/multimodal_encoder/dinov2_encoder.py
@@ -14,7 +14,12 @@ class Dinov2VisionTower(nn.Module):
         # For Dinov2, we set select_layer to -2
         self.select_layer = getattr(args, 'mm_vision_select_layer', -2)
         self.select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
-        
+
+        # Use AutoImageProcessor for Dinov2
+        img_size = 518
+        self.image_processor = AutoImageProcessor.from_pretrained(self.vision_tower_name,
+                                                              size={"height": img_size, "width": img_size}, crop_size={"height": img_size, "width": img_size})
+
         if not delay_load:
             self.load_model()
         elif getattr(args, 'unfreeze_mm_vision_tower', False):
@@ -27,9 +32,6 @@ class Dinov2VisionTower(nn.Module):
             print('{} is already loaded, `load_model` called again, skipping.'.format(self.vision_tower_name))
             return
 
-        # Use AutoImageProcessor for Dinov2
-        self.image_processor = AutoImageProcessor.from_pretrained(self.vision_tower_name,
-                                                              size={"height": 768, "width": 768}, crop_size={"height": 768, "width": 768})
         # self.image_processor.crop_size['height'] = self.image_processor.crop_size['width'] = 768
 
         self.vision_tower = Dinov2Model.from_pretrained(self.vision_tower_name,device_map=device_map)
@@ -51,7 +53,7 @@ class Dinov2VisionTower(nn.Module):
 
     @torch.no_grad()
     def forward(self, images): # Forward pass to extract features. Handles both single tensor and list of tensors.
-        raise RuntimeError(f"PPPPPPPPPPPPPPPPPTTTTTTT, shape={str(images.shape)}")
+        # raise RuntimeError(f"PPPPPPPPPPPPPPPPPTTTTTTT, shape={str(images.shape)}")
 
         if type(images) is list:
             image_features = []
